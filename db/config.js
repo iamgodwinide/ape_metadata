82da7468b2848411f2f7dcec900877ac9eddb163
Godwin Idemudia <godwinide@gmail.com>
2022-10-15 15:32:33 +0100

    new

--- db/config.js
+++ db/config.js
@@ -2,6 +2,6 @@ const { connect } = require("mongoose");
 
 
 
-connect("mongodb://127.0.0.1:27017/hekokokkklljuo12")
+connect("mongodb://127.0.0.1:27017/hekokodhdhdhkkklljuo12")
     .then(() => console.log("MongoDB Connected"))
     .catch(err => console.log(err));
